151109ae2dd8f56b5b30748afd7d707d99f062bf
Muhammad-Rayhan <rayhan.cse2k17@gmail.com>
2022-09-28 21:13:23 +0600

    Javascript Practice 09/28/2022

---
+++ 04_Javascript_Constant.js
@@ -0,0 +1,24 @@
+"use strict"; //Most Importent for Showing Error
+
+// let myName = "Rayhan";
+// let myFriendName = "Salman";
+
+// myName = myFriendName;
+
+// console.log(myName);
+// console.log(myFriendName);
+
+
+/*
+========================================================================
+========================================================================
+Note: Most Of The Developer Use const Variable Name Uppercase (MYNAME),
+And When Variable Name is So Long Than Use Underscore (MY_COLLEGE_NAME),
+Not Use Camelcase (It's Not Standerd)
+========================================================================
+========================================================================
+*/
+
+// Constant Means Fixed, MY_NAME Variable Not Declear Any More. When Declear MY_NAME Second Time Than Show a Error Message.
+const MY_NAME = "Rayhan";
+console.log(MY_NAME);
---
+++ 06_Javascript_Backtick_Concat.js
@@ -0,0 +1,23 @@
+"use strict" //Most Importent for Showing Error
+
+// String Type
+let str1 = 'First String';
+let str2 = "Second String";
+let str3 = `Third String`;
+
+let num1 = 10;
+let num2 = 20;
+console.log(`${num1 + num2}`);
+
+
+// Ptint a Message Like (My Name is userName, I Am 23 Years UserAge.) 
+let userName = "Rayhan";
+let userAge = 23;
+
+// Concatenation Method
+let message = "My Name is "+userName+", I Am "+userAge+" Years Old.";
+console.log(message);
+
+//Backtick Method
+let message2 = `My Name is ${userName}, I Am ${userAge} Years Old.`;
+console.log(message2);
