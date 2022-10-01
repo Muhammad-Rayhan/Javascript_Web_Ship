542e4c483cd771916dbdf81c1949136e71fa6b67
Muhammad-Rayhan <rayhan.cse2k17@gmail.com>
2022-10-01 20:42:10 +0600

    Javascript Practice 10/01/2022

---
+++ 11_Javascript_Operator.js
@@ -0,0 +1,10 @@
+// 1,2,3... Operand
+// +,-,*,/... Operator
+
+/* Unarry Operator: Je Shokol Operator a Akti Matro Operand Thake Take Unarry Operator Bole.
+Example: -5 (Nigation Operator) (-) ar Pore Operand Akti Thakle Atake Nigation Operator Bole
+*/
+
+/* Binary Operator: Je Shokol Operator Duita Operand Niye Kaj kore Take Binary Operator Bole.
+Example: 5 + 5 
+*/
