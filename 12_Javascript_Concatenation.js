3d1b584abc8fd71f9a29cf2e11e7ced6f1e8141f
Muhammad-Rayhan <rayhan.cse2k17@gmail.com>
2022-10-02 07:40:09 +0600

    Javascript Practice 10/02/2022

---
+++ 12_Javascript_Concatenation.js
@@ -0,0 +1,22 @@
+"use strict"
+/*
+# When Only (+) Concatenation Number & String Number Than Return String Value, 
+# Otherwise Number (-, /, *, %) String Number Than Javascript Automatic Convert String Number To Number & Calculation Than Return Value. Example: (10 - "3") / (10 * "3") / (10 / "3") / (10 % "3")
+
+let num1 = 3 + "4";
+console.log(num1); //Return 34
+*/
+
+/*
+# At First Additional Operator Calculation First Two Number Operand (2 + 6 = 8) Than The Number Concatanation Last String Number (8 + "8" = 88)
+
+let num2 = 2 + 6 + "8";
+console.log(num2); //Return 88
+*/
+
+/*
+# When Addition Start String Number or Number Than Concatination This Two Value & Return String Value ("2" + 6 = "26") Than Concatanation String Number ("26") to Last Number & Return Again String Value ("26" + 8 = 268)
+
+let num2 = "2" + 6 + 8;
+console.log(num2); //Return 268
+*/
