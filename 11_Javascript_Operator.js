61e874ddb5a8f8ebdd01858130046fa14748100d
Muhammad-Rayhan <rayhan.cse2k17@gmail.com>
2022-10-12 23:18:02 +0600

    Javascript Practice 10/12/2022

--- 11_Javascript_Operator.js
+++ 11_Javascript_Operator.js
@@ -10,11 +10,32 @@ Example: 5 + 5
 */
 
 // JavaScript Comparison Operators
-//Equal To 
 let num = 2;
 let num2 = 2;
 let num3 = 5;
 let strNum = "8";
 let strNum2 = 8;
+let str = "Rayhan";
 
-console.log(strNum + strNum2);
+// console.log(strNum + strNum2); //return 88
+
+// console.log(strNum == strNum2); //true
+// console.log(strNum == num3); //false
+// console.log(strNum == str); //false
+
+// console.log(strNum != strNum2); //if first variable value & second variable value is same than return false else return true
+// console.log(str != num); //true
+
+console.log(num > num2); //false
+console.log(num < num2); //false
+console.log(num >= num2); //true
+console.log(num <= num2); //true
+console.log(num > str); //false
+console.log(strNum > num); //true
+
+console.log(num === num2); //true if type or value is same than return true else return false 
+console.log(strNum === strNum2); //false
+
+console.log(num !== num2); //false if type or value is same than return false else return true 
+console.log(strNum !== strNum2); //true if type or value not equil than return true
+console.log(num !== num3); //true
